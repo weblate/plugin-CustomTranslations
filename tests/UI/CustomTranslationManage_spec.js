e29e7878c1fdbf8cfec16f3e549ae70d87ca5f9a
sgiehl <stefan@matomo.org>
2020-07-28 13:49:44 +0200

    convert UI tests to headless chrome

--- tests/UI/CustomTranslationManage_spec.js
+++ tests/UI/CustomTranslationManage_spec.js
@@ -21,7 +21,7 @@ describe("CustomTranslationManage", function () {
         testEnvironment.save();
     });
 
-    function setTranslation(page, type, row, translation, setKey)
+    async function setTranslation(page, type, row, translation, setKey)
     {
         var fieldClass = '.fieldUiControl2';
         if (setKey) {
@@ -29,45 +29,38 @@ describe("CustomTranslationManage", function () {
         }
 
         var selector = '.translationType' + type + ' .multiPairFieldTable' + row + ' '+ fieldClass + ' .control_text';
-        page.execCallback(function () {
-            page.webpage.evaluate(function(selector) {
-                $(selector).val('').change();
-            }, selector);
+        await page.webpage.evaluate(function(selector) {
+            $(selector).val('').change();
+        }, selector);
 
-            page.webpage.evaluate(function(selector, text) {
-                $(selector).val(text).change();
-            }, selector, translation);
-        });
+        await page.webpage.evaluate(function(selector, text) {
+            $(selector).val(text).change();
+        }, selector, translation);
     }
 
-    function saveTranslation(page, type)
+    async function saveTranslation(page, type)
     {
-        page.click('.translationType' + type + ' [piwik-save-button] .btn');
+        await page.click('.translationType' + type + ' [piwik-save-button] .btn');
     }
 
-    it('should load manage page', function (done) {
-        expect.screenshot('loaded').to.be.captureSelector('.pageWrap', function (page) {
-            page.load(url);
-        }, done);
+    it('should load manage page', async function () {
+        await page.goto(url);
+        expect(await page.screenshotSelector('.pageWrap')).to.matchImage('loaded');
     });
 
-    it('should be possible to enter values', function (done) {
-        expect.screenshot('values_entered').to.be.captureSelector('.pageWrap', function (page) {
-            setTranslation(page, 'customDimensionEntity', 0, 'newValue1');
-            setTranslation(page, 'customDimensionEntity', 1, 'newKey2');
-            setTranslation(page, 'customDimensionEntity', 1, 'newValue2', true);
-            saveTranslation(page, 'customDimensionEntity');
-
-            setTranslation(page, 'eventLabel', 0, 'eventVal1');
-            setTranslation(page, 'eventLabel', 2, 'eventKey');
-            setTranslation(page, 'eventLabel', 2, 'eventVal2', true);
-            saveTranslation(page, 'eventLabel');
-        }, done);
+    it('should be possible to enter values', async function () {
+        await setTranslation(page, 'customDimensionEntity', 0, 'newValue1');
+        await setTranslation(page, 'customDimensionEntity', 1, 'newKey2');
+        await setTranslation(page, 'customDimensionEntity', 1, 'newValue2', true);
+        await saveTranslation(page, 'customDimensionEntity');
+        await setTranslation(page, 'eventLabel', 0, 'eventVal1');
+        await setTranslation(page, 'eventLabel', 2, 'eventKey');
+        await setTranslation(page, 'eventLabel', 2, 'eventVal2', true);
+        await saveTranslation(page, 'eventLabel');
     });
 
-    it('should show save values on reload', function (done) {
-        expect.screenshot('values_saved_verify').to.be.captureSelector('.pageWrap', function (page) {
-            page.load(url);
-        }, done);
+    it('should show save values on reload', async function () {
+        await page.goto(url);
+        expect(await page.screenshotSelector('.pageWrap')).to.matchImage('values_saved_verify');
     });
 });
